de229c5e98983ec36b015259b6984f8182bd8b20
Ahmet Unal <87841677+zanileberkay@users.noreply.github.com>
2024-04-19 17:46:07 -0400

    Update server.py

--- server.py
+++ server.py
@@ -97,4 +97,4 @@ def complete_registration():
         return jsonify({'error': 'Internal Server Error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))#
